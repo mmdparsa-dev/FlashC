543e50ec95c541d3c89f305606dba79f7a7c5e12
mmdparsa <sydmhmdparsahajyrdwyynyn@gmail.com>
2026-06-29 17:51:58 +0330

    Fix repository settings in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,6 @@
 pluginManagement {
   repositories {
-    maven { url = uri("https://maven.myket.ir/") }
-    maven { url = uri("https://mirror-maven.runflare.com/maven2") }
-    maven { url = uri("https://maven.chrepo.ir") }
-    maven { url = uri("https://mirror.kargadan.ir/repository/maven-central-group/") }
-    /*google {
+ google {
       content {
         includeGroupByRegex("com\\.android.*")
         includeGroupByRegex("com\\.google.*")
@@ -12,7 +8,7 @@ pluginManagement {
       }
     }
     mavenCentral()
-    gradlePluginPortal()*/
+    gradlePluginPortal()
   }
 }
 
@@ -21,12 +17,8 @@ plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 dependencyResolutionManagement {
   repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
   repositories {
-    maven { url = uri("https://maven.myket.ir/") }
-    maven { url = uri("https://mirror-maven.runflare.com/maven2") }
-    maven { url = uri("https://maven.chrepo.ir") }
-    maven { url = uri("https://mirror.kargadan.ir/repository/maven-central-group/") }
-    /* google()
-     mavenCentral()*/
+     google()
+     mavenCentral()
   }
 }
 
